02bbfe0063621ccd75e9b4b56aeefdf9a037d623
Sanya Sharma <sanyaasharma3241@gmail.com>
2024-02-26 23:50:01 +0530

    Api integration and data fetching

--- backend/Controllers/doctorController.js
+++ backend/Controllers/doctorController.js
@@ -53,3 +53,23 @@ export const getAllDoctor = async(req, res)=>{
         res.status(404).json({success: false, message: 'Not found' });
     }
 };
+
+export const getDoctorProfile = async(req, res)=>{
+    const doctorId = req.userId;
+
+    try{
+        const doctor = await Doctor.findById(doctorId)
+
+        if(!doctor){
+            return res.status(404).json({success: false, message: 'doctor not found' });
+        }
+
+        const {password, ...rest} = doctor._doc ;
+        const appointments = await Booking.find({doctor: doctorId})
+
+        res.status(200).json({success:true, message:'Profile info is getting', data:{...rest}})
+
+    }catch(err){
+        res.status(500).json({success: false, message: "Something went wrong, cannot get"});
+    }
+}
