30bcc1a6961a2d3673b1a3062e916aa2a50cd13e
Sanya Sharma <sanyaasharma3241@gmail.com>
2024-02-29 00:59:07 +0530

    Profile Updation Functionality Added to doctor profile

--- frontend/src/Dashboard/doctor-account/Dashboard.jsx
+++ frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -12,7 +12,7 @@ const Dashboard = () => {
 
   const {data, loading, error} = useFetchData(`${BASE_URL}/doctors/profile/me`);
 
-  const [tab, setTab] = useState('appointments');
+  const [tab, setTab] = useState('overview');
   return( <section>
     <div className="max-w-[1170px] px-5 mx-auto">
       {loading && !error && <Loader/>}
@@ -22,7 +22,47 @@ const Dashboard = () => {
         !loading && !error &&(
            <div className="grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]">
             <Tabs tab={tab} setTab={setTab}/>
-            
+            <div className="lg: col-span-2">
+
+            <div className="mt-8">
+                {tab==='overview' && (
+                <div>
+                  <div className="flex items-center gap-4 mb-10">
+                    <figure className="max-w-[200px] max-h-[200px]"><img src={data?.photo} alt="" className="w-full"/>
+                    </figure>
+                    <div>
+                      <span className="bg-[#CCF0F3] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold">
+                        {data.specialization}
+                      </span>
+
+                      <h3 className="text-[22px] leading-9 font-bold text-headingColor mt-3">{data.name}</h3>
+
+                      <div className="flex items-center gap-[6px]">
+                        <span className="flex items-center gap-[6px] text-headingColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold">
+                          <img src={starIcon} alt=""/>
+                          {data.averageRating}
+                          </span>
+                          <span className=" text-headingColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold">
+                          ({data.totalRating})
+                          </span>
+                      </div>
+                      <p className="text__para font-[15px] lg:max-w-[390px] leading-6" >
+                       {data?.bio}
+                        </p>
+                      </div>
+                    </div>
+                    <DoctorAbout 
+                    name={data.name}
+                    about={data.about}
+                    qualifications={data.qualifications}
+                    experiences={data.experiences}/>
+                    </div>
+
+                    )}
+                {tab==='appointments' && <div>appointments</div>}
+                {tab==='settings' && <Profile doctorData={data}/>}
+                </div>
+            </div>
           </div>
       )}
     </div>
--- frontend/src/Dashboard/doctor-account/Tabs.jsx
+++ frontend/src/Dashboard/doctor-account/Tabs.jsx
@@ -1,51 +1,69 @@
-import {useContext} from 'react'
+import { useContext } from 'react';
 import { BiMenu } from 'react-icons/bi';
 import { authContext } from '../../context/AuthContext';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
-const Tabs = ({tab, setTab}) => {
-    const dispatch = useContext(authContext)
-    const navigate = useNavigate()
+const Tabs = ({ tab, setTab }) => {
+    const {dispatch} = useContext(authContext);
+    const navigate = useNavigate();
 
-    const handleLogout = ()=>{
-        dispatch({type: 'LOGOUT'})
-        navigate('/')
-    }
-  return (
-    <div>
-    <span className="lg:hidden">
-        <BiMenu className="w-6 h-6 cursor-pointer" />
-    </span>
-    <div className="hidden lg:flex flex-col p-[30px] shadow-panelShadow items-center h-max rounded-md">
-        <button
-            className={`${
-                tab === "appointments"
-                    ? "bg-indigo-100 text-primaryColor"
-                    : "bg-transparent text-headingColor"
-            } w-full btn mt-0 rounded-md`}
-            onClick={() => setTab('appointments')}
-        >
-            Appointments
-        </button>
-        <button
-            className={`${
-                tab === "settings"
-                    ? "bg-indigo-100 text-primaryColor"
-                    : "bg-transparent text-headingColor"
-            } w-full btn mt-0 rounded-md`}
-            onClick={() => setTab('settings')}
-        >
-            Profile
-        </button>
-        <div className="w-full mt-[100px]">
-                        <button onClick={handleLogout} className="w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white mb-4">Logout</button>
-                        <button className="w-full bg-red-600 p-3 text-[16px] leading-7 rounded-md text-white">Delete account</button>
+    const handleLogout = () => {
+        console.log('btn');
+        dispatch({ type: "LOGOUT" });
+        navigate("/login"); // Corrected this line
+    };
 
+    return (
+        <div>
+            <span className="lg:hidden">
+                <BiMenu className="w-6 h-6 cursor-pointer" />
+            </span>
+            <div className="hidden lg:flex flex-col p-[30px] shadow-panelShadow items-center h-max rounded-md">
+            <button
+    className={`${
+        tab === 'overview'
+            ? 'bg-indigo-100 text-primaryColor'
+            : 'bg-transparent text-headingColor'
+    } w-full btn mt-0 rounded-md`}
+    onClick={() => setTab('overview')}
+>
+    Overview
+</button>
+
+                <button
+                    className={`${
+                        tab === 'appointments'
+                            ? 'bg-indigo-100 text-primaryColor'
+                            : 'bg-transparent text-headingColor'
+                    } w-full btn mt-0 rounded-md`}
+                    onClick={() => setTab('appointments')}
+                >
+                    Appointments
+                </button>
+                <button
+                    className={`${
+                        tab === 'settings'
+                            ? 'bg-indigo-100 text-primaryColor'
+                            : 'bg-transparent text-headingColor'
+                    } w-full btn mt-0 rounded-md`}
+                    onClick={() => setTab('settings')}
+                >
+                    Profile
+                </button>
+                <div className="w-full mt-[100px]">
+                    <button
+                        onClick={handleLogout}
+                        className="w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white mb-4"
+                    >
+                        Logout
+                    </button>
+                    <button className="w-full bg-red-600 p-3 text-[16px] leading-7 rounded-md text-white">
+                        Delete account
+                    </button>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
+    );
+};
 
-  )
-}
-
-export default Tabs
+export default Tabs;
